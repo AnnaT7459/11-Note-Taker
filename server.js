a2b19eb72299b5cd9c1d8dddd0da43a20ed3b961
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2023-12-03 16:57:56 -0600

    edited get methods to define the routes for the landing/notes page

--- server.js
+++ server.js
@@ -21,11 +21,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
   });
+  
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+
+
