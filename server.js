7102f380c35306630a34cb0a670133127c6b79ed
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2023-12-03 19:43:26 -0600

    refactored index.js to try saving notes to left-hand column: needs debugged

--- server.js
+++ server.js
@@ -23,43 +23,45 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
 app.get('/api/notes', (req, res) => {
     res.status(200).json(notes);
-  });
+});
 
 //   post request to add a note
 app.post('/api/notes', (req, res) => {
     // log that post was received
     console.info(`${req.method} request received to add a note`);
 
-// destructure for items in req.body
-const { title, text } = req.body;
+    // destructure for items in req.body
+    const { title, text } = req.body;
 
-// if note title and text are/are not entered:
-if (title && text) {
-    const newNote = {
-        title,
-        text,
-        review_id: uuid(),
-    };
+    // if note title and text are/are not entered:
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            review_id: uuid(),
+        };
 
-    const userNote = {
-        status:'success',
-        body:  newNote,
-    };
-    console.log(userNote);
-    // reference: "HTTP response status codes" mdm web docs: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-    // 201 = 'created'
-    res.status(201).json(userNote);
-} else {
-    // 500 = internal server error
-    res.status(500).json('Error adding note')
-}
+        const userNote = {
+            status: 'success',
+            body: newNote,
+        };
+        console.log(userNote);
+        // reference: "HTTP response status codes" mdm web docs: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+        // 201 = 'created'
+        res.status(201).json(userNote);
+    } else {
+        // 500 = internal server error
+        res.status(500).json('Error adding note')
+    }
 })
-  
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    });
 
-
-;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
