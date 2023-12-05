b63b29ca51254ffd75f40d1c84a84ea51a611f30
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2023-12-04 21:55:32 -0600

    completed bonus challenge of deleting a note using the app.delete function and parameters

--- server.js
+++ server.js
@@ -40,6 +40,36 @@ app.get('/api/notes', (req, res) => {
     });
 });
 
+// reference: "Express.js – app.delete() Method" Tutorials Point: https://www.tutorialspoint.com/express-js-app-delete-method
+// colon is a placeholder for the route to choose any of the object in the array based off the id
+app.delete('/api/notes/:id', (req, res) => {
+    // elete request received
+    console.info(`${req.method} request received to delete a note`);
+  
+    // reference: "req.query and req.param in ExpressJS" Stack Overflow: https://stackoverflow.com/questions/18524125/req-query-and-req-param-in-expressjs
+    // the request pulls the object based of the id so it knows which note to delete
+    const noteId = req.params.id;
+  
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        res.status(500).json(err);
+      } else {
+        // Converts string into JSON object
+        const parsedNotes = JSON.parse(data);
+        // takes the deleted note out of the array from the other notes
+        const updatedNotes = parsedNotes.filter((note) => note.id !== noteId);
+  
+        fs.writeFile('./db/db.json', JSON.stringify(updatedNotes), (err, data) => {
+          if (err) {
+            res.status(500).json(err);
+          } else {
+            res.status(200).json({ status: 'success' });
+          }
+        });
+      }
+    });
+  });
+
 //   post request to add a note
 app.post('/api/notes', (req, res) => {
     // log that post was received
@@ -83,7 +113,7 @@ app.post('/api/notes', (req, res) => {
         // bad request = 400
         res.status(400).json('Enter a title and text for your note');
     }
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
